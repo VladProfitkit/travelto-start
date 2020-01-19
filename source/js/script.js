eded678a8d2d03146d27df8168f9908c5f2e5772
schegol <48925789+schegol@users.noreply.github.com>
2020-01-19 19:03:32 +0300

    Завершение верстки футера

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
     var phoneInfo = $('.header-top__phone-wrapper');
     var menuButtonMobile = $('.header-top__mobile-menu-toggle');
     var mainMenuMobile = $('.header-top__mobile-menu-container');
+    var footerMenuColumn = $('.footer__column-list');
+    var footerMenuToggle = $('.footer__column-heading');
+
+    //приведение колонок навигации в футере на мобильных в интерактивное состояние при включенном js
+    function hideFooterMenu() {
+      footerMenuColumn.removeClass('footer__column-list--open');
+      footerMenuToggle.removeClass('footer__column-heading--no-js');
+      footerMenuToggle.removeClass('footer__column-heading--open');
+    }
 
     function closePhoneInfo() {
        phoneInfo.removeClass('header-top__phone-wrapper--open');
@@ -12,13 +21,26 @@ $(document).ready(function() {
        mainMenuMobile.removeClass('header-top__mobile-menu-container--open');
     }
 
-    phoneButtonMobile.click(function(e){
+    hideFooterMenu();
+
+    //сворачивание колонок навигации в футере на мобильных
+    footerMenuToggle.each(function() {
+      $(this).click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('footer__column-heading--open');
+        $(this).next(footerMenuColumn).toggleClass('footer__column-list--open');
+      });
+    });
+
+    //открытие контактов в хедере на мобильных
+    phoneButtonMobile.click(function(e) {
       e.preventDefault();
       closeMobileMenu();
       phoneInfo.toggleClass('header-top__phone-wrapper--open');
     });
 
-    menuButtonMobile.click(function(e){
+    //открытие меню на мобильных
+    menuButtonMobile.click(function(e) {
       e.preventDefault();
       closePhoneInfo();
       mainMenuMobile.toggleClass('header-top__mobile-menu-container--open');
